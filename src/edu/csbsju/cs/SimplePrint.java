10d697bd0c2ff85accd2f6b344d98129ace3fd60
V1XIONG <v1xiong@vplincsci079.ad.csbsju.edu>
2018-03-13 13:43:34 -0500

    Init commit

---
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.csbsju.cs;
+
+/**
+ * @author v1xiong
+ *
+ */
+public class SimplePrint {
+
+	/**
+	 * 
+	 */
+	public SimplePrint() {
+		// TODO Auto-generated constructor stub
+	}
+private String pName()
+{
+	
+}
+
+private int pAge()
+{
+	
+}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Xiong");
+    for(int i = 0;i<args.length;i++)
+    {
+    	System.out.println("args["+ i +"] ="+ args[i]);
+    }
+	}
+
+}
